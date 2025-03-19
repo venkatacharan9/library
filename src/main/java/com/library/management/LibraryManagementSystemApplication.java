e658596c13efa4deda4523d79a14cbafe8e45d8e
venkatacharan9 <venkatcharantej9@gmail.com>
2025-03-19 11:48:25 +0100

    added @EnableAsync annotation

--- src/main/java/com/library/management/LibraryManagementSystemApplication.java
+++ src/main/java/com/library/management/LibraryManagementSystemApplication.java
@@ -3,12 +3,14 @@ package com.library.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
+@EnableAsync
 public class LibraryManagementSystemApplication {
 
 	public static void main(String[] args) {
